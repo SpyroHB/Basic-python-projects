73c9e3cc8e6159f065689e52e966109bb13f018e
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-01 15:48:51 +0100

    Hiii

---
+++ Project.py
@@ -0,0 +1,13 @@
+from tkinter import N
+
+
+Project_greeting="Hello World \nI'm New Project Created by:~~\nSpyroHB ~~ xD"
+print(Project_greeting)
+
+patient_name= 'John Smith'
+patient_age=20
+patient_exsiting = True, False 
+print(patient_name,'\n',patient_age,'\n',patient_exsiting)
+
+name= input('What is your name? ')
+print('Hi ' + name)
