17b67f559fda368cbf9b35c6e8bfdb5b64d2688f
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-01 23:38:52 +0100

    Done

--- Project.py
+++ Project.py
@@ -1,7 +1,7 @@
 from ast import Await
 from multiprocessing.connection import wait
 from tkinter import N
-
+import math 
 # Printing a Simple Project Greeting!
 
 Project_greeting='''Hello World! 
@@ -9,19 +9,24 @@ I'm New Project Created
 by:
 ~~ SpyroHB ~~ xD'''
 
-# Learning Basic  Input & Conversion "Strings and Int/float"
-
-Q1_Fname= input('What is your first name? ')
-Q1_Lname= input('What is your Last name? ')
-Q1_age= input('What is your Birth year? ')
-age= 2022 - int(Q1_age)
-Q1_weight_pounds= input('What is your Weight in Lbs: ')
-Q1_weight_kilograms = float(Q1_weight_pounds) // 2.205
-msg= f'''Full Name: {Q1_Fname} {Q1_Lname}.
+# Learning Basic  Input & Conversion "Strings and Int/float".
+# Inputing your User's first name.
+U1_Fname= input('What is your first name? ')
+# Inputing your User's second name.
+U1_Lname= input('What is your Last name? ')
+# Inputing your User's Age.
+U1_age= input('What is your Birth year? ')
+# Coversing a String with int and subtracting 
+age= 2022 - int(U1_age)
+# Inputing your User's weight 
+U1_weight_pounds= input('What is your Weight in Lbs: ')
+# Conversing a String with float/int by using integer dividing "1 kilogram = 2.205 kbs"
+U1_weight_kilograms = float(Q1_weight_pounds) // 2.205
+# Formatted string so we orgnaize our output message
+msg= f'''Full Name: {U1_fname} {U1_Lname}.
 Age:{age} years old
-Weight in Kilograms:{Q1_weight_kilograms}
+Weight in Kilograms:{U1_weight_kilograms}
 '''
-
 # Using a basic output "Terminal"
 print(Project_greeting)
-print(msg)          
+print(msg)   
