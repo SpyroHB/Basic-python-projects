ef1d16dce1e5c82accc54c812fbcd455b1265afd
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-01 16:10:34 +0100

    hi

--- Project.py
+++ Project.py
@@ -9,5 +9,8 @@ patient_age=20
 patient_exsiting = True, False 
 print(patient_name,'\n',patient_age,'\n',patient_exsiting)
 
-name= input('What is your name? ')
-print('Hi ' + name)
+Q1_name= input('What is your name? ')
+Q1_age= input('What is your Age? ')
+print('Name:', Q1_name, '\nAge:', Q1_age)
+
+
