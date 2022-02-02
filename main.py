f1bd3a6fe53e79151c134e9466f26b39a52145a7
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-02 14:48:21 +0100

    Done

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 # Printing a Simple Project Greeting!
+from ast import Return
+from tkinter import CURRENT
+from datetime import datetime, time
 Project_greeting='''Hello World! 
 I'm New Project Created 
 by:
 ~~ SpyroHB ~~ xD'''
-
+date_err = 'Please use a valid date!'
 # Learning Basic  Input & Conversion "Strings and Int/float".
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Inputing your User's first name.
 U1_Fname= input('What is your first name? ')
 # Inputing your User's second name.
 U1_Lname= input('What is your Last name? ')
-# Inputing your User's Age.
-U1_age= input('What is your Birth year? ')
+# Inputing your User's Birthday.
+U1_date= input('What is your birthday? ')
 # Coversing a String with int and subtracting 
-age= 2022 - int(U1_age)
+U1_day= U1_date[0:2]
+U1_month= U1_date[2:4]
+U1_year= U1_date[4:]
+
+date= 2022 - int(U1_year)
 # Inputing your User's weight 
 U1_weight_pounds= input('What is your Weight in Lbs: ')
 # Conversing a String with float/int by using integer dividing "1 kilogram = 2.205 kbs"
 U1_weight_kilograms = float(U1_weight_pounds) // 2.205
 # Formatted string so we orgnaize our output message
 msg= f'''Full Name: {U1_Fname} {U1_Lname}.
-Age:{age} years old
+date:{date} years old
+Birthday: {U1_day}/{U1_month}/{U1_year}.
 Weight in Kilograms:{U1_weight_kilograms}
 '''
 # Using a basic output "Terminal"
