217e25cd16efaa3945491435105cdcf570834fb7
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-07 17:07:52 +0100

    Updated

--- .py
+++ .py
@@ -5,7 +5,11 @@ MyNamesContainer = {
 "Jack" : 5,
 "Alex" : 12,
 "John" : 18}
-if name in MyNamesContainer:
-	print("Found it! It is corresponds to", 
-		MyNamesContainer[name])
-else: print("No such name in the container.")
+shit = 0
+while True:
+	if name in list(MyNamesContainer):
+		print("Found it! It is corresponds to",MyNamesContainer[name])
+		break
+	if name not in MyNamesContainer:
+			print("No such name in the container.")
+	name = input("Enter a name:")
