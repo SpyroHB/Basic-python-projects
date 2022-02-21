e8d336c68e90e75d7f761bda28a61c01e0ce3531
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-21 18:05:58 +0100

    Updated + Added Discord Bot written in python

---
+++ python_functions/emoji_converter.py
@@ -0,0 +1,15 @@
+def emoji_converter(msg):
+    words = msg.split(' ')
+    emojis = {
+        "<3": "♥",
+        ":)": "☺"
+    }
+    output= ''
+    for word in words:
+        output += emojis.get(word, word) + " "
+    return output.capitalize()
+
+
+msg = input('')
+print(emoji_converter(msg))
+
