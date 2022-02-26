e2a9e43277575c4f9e0f78fdd41e821be6230366
ProSDLMFDQFK <businessblueox@gmail.com>
2022-02-26 23:47:08 +0100

    Added basic Calc built with Functions

---
+++ Basic_CalcGUI/config.py
@@ -0,0 +1,22 @@
+msg = "multiplication (*)\naddition (+)\nsubtraction (-)\nexponent (**)\ndivision (/)\ntype 'quit' to exit\n:>  "
+
+
+def exponent(num1,num2):
+    return num1**num2
+
+
+def addition(num1,num2):
+    return num1+num2
+
+
+def subtraction(num1,num2):
+    return num1-num2
+
+
+def multiplication(num1,num2):
+    return num1*num2
+
+
+def division(num1,num2):
+    return num1/num2
+
---
+++ Basic_CalcGUI/main.py
@@ -0,0 +1,44 @@
+from config import addition , subtraction, division , multiplication , exponent
+
+def double_space():
+    print()
+    print()
+
+
+msg = "multiplication (*)\naddition (+)\nsubtraction (-)\nexponent (**)\ndivision (/)\ntype 'quit' to exit\n:>  "
+
+z = input(msg).lower()
+double_space()
+while not z == 'quit':
+    if z == '+':
+        n1 = float(input('type first number: '))
+        n2 = float(input('type second number: '))
+        double_space()
+        print(f'{n1} + {n2} = ',addition(n1,n2))
+        double_space()
+    elif z == '-':
+
+        n1 = float(input('type first number: '))
+        n2 = float(input('type second number: '))
+        double_space()
+        print(f'{n1} - {n2} = ',subtraction(n1,n2))
+        double_space()
+    elif z == '*':
+        n1 = float(input('type first number: '))
+        n2 = float(input('type second number: '))
+        double_space()
+        print(f'{n1} * {n2} = ',multiplication(n1,n2))
+        double_space()
+    elif z == '**':
+        n1 = float(input('type first number: '))
+        n2 = float(input('type second number: '))
+        double_space()
+        print(f'{n1} ** {n2} = ',exponent(n1,n2))
+        double_space()
+    elif z == '/':
+        n1 = float(input('type first number: '))
+        n2 = float(input('type second number: '))
+        double_space()
+        print(f'{n1} / {n2} = ',division(n1,n2))
+        double_space()
+    z = input(msg).lower()
