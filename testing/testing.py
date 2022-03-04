9607e9619c277b6e42bd8187f82233b5fa6d9bce
ProSDLMFDQFK <businessblueox@gmail.com>
2022-03-04 21:57:31 +0100

    Updated

--- testing/testing.py
+++ testing/testing.py
@@ -1,1 +1,10 @@
+def histogram(s):
+    d = dict()
+    for c in s:
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    print(d)
 
+histogram('younes himda aka the fucking spyro')
