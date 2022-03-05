4c6cdf2538f11eb8bc6e52e3c53004c86a66b5ac
ProSDLMFDQFK <businessblueox@gmail.com>
2022-03-05 19:51:54 +0100

    Updated

--- python_functions/emoji_converter.py
+++ python_functions/emoji_converter.py
@@ -1,17 +1,16 @@
 def emoji_converter(msg):
     words = msg.split(' ')
-    emojis = {
+    emojis_disc = {
         "<3": "♥",
-        ":)": "☺"
+        ":)": "☺",
+        ":(": "☺"
     }
     output= ''
     for word in words:
-        output += emojis.get(word, word) + " "
+        output += emojis_disc.get(word, word) + " "
     return output.capitalize()
 
 
 msg = input('')
 print(emoji_converter(msg))
-
-
 
