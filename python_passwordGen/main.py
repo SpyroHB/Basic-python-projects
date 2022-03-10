563f6d0b394e684abac37217e70b1d7eded77ea3
ProSDLMFDQFK <businessblueox@gmail.com>
2022-03-10 20:44:54 +0100

    Updated

--- python_passwordGen/main.py
+++ python_passwordGen/main.py
@@ -1,9 +1,7 @@
-import string as s
-import random as r
-
-character = list(s.ascii_letters + s.digits + "+*/-().")
-
-def generate_random_password(passLength=int):
+def generate_random_password(passLength=int,):
+    import string as s
+    import random as r
+    character = list(s.ascii_letters + s.digits + "+*/-().")
     password = []
     if passLength > 30:
         return 'Your password length must be smaller or equal to 30 characters!'
@@ -13,6 +11,3 @@ def generate_random_password(passLength=int):
             password.append(r.choice(character))
             r.shuffle(password)
     return "".join(password)
-
-
-
