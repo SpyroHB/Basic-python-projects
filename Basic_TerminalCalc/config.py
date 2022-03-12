ddce3480103307d9c572a74650769a4fb11d1c34
ProSDLMFDQFK <businessblueox@gmail.com>
2022-03-12 15:02:08 +0100

    Updated "Added Class"

--- Basic_TerminalCalc/config.py
+++ Basic_TerminalCalc/config.py
@@ -1,22 +1,26 @@
 msg = "multiplication (*)\naddition (+)\nsubtraction (-)\nexponent (**)\ndivision (/)\ntype 'quit' to exit\n:>  "
 
 
-def exponent(num1,num2):
-    return num1**num2
+class CalcuOptions:
+    def __init__(self) -> None:
+        pass
+        
+
+    def exponent(num1,num2):
+        return num1**num2
 
 
-def addition(num1,num2):
-    return num1+num2
+    def addition(num1,num2):
+        return num1+num2
 
 
-def subtraction(num1,num2):
-    return num1-num2
+    def subtraction(num1,num2):
+        return num1-num2
 
 
-def multiplication(num1,num2):
-    return num1*num2
+    def multiplication(num1,num2):
+        return num1*num2
 
 
-def division(num1,num2):
-    return num1/num2
-
+    def division(num1,num2):
+        return num1/num2
--- Basic_TerminalCalc/main.py
+++ Basic_TerminalCalc/main.py
@@ -1,4 +1,4 @@
-from config import addition , subtraction, division , multiplication , exponent
+from config import CalcuOptions
 
 def double_space():
     print()
@@ -16,30 +16,30 @@ while not z == 'quit':
         n1 = float(input('type first number: '))
         n2 = float(input('type second number: '))
         double_space()
-        print(f'{n1} + {n2} = ',addition(n1,n2))
+        print(f'{n1} + {n2} = ',CalcuOptions.addition(n1,n2))
         double_space()
     elif z == '-':
         n1 = float(input('type first number: '))
         n2 = float(input('type second number: '))
         double_space()
-        print(f'{n1} - {n2} = ',subtraction(n1,n2))
+        print(f'{n1} - {n2} = ',CalcuOptions.subtraction(n1,n2))
         double_space()
     elif z == '*':
         n1 = float(input('type first number: '))
         n2 = float(input('type second number: '))
         double_space()
-        print(f'{n1} * {n2} = ',multiplication(n1,n2))
+        print(f'{n1} * {n2} = ',CalcuOptions.multiplication(n1,n2))
         double_space()
     elif z == '**':
         n1 = float(input('type first number: '))
         n2 = float(input('type second number: '))
         double_space()
-        print(f'{n1} ** {n2} = ',exponent(n1,n2))
+        print(f'{n1} ** {n2} = ',CalcuOptions.exponent(n1,n2))
         double_space()
     elif z == '/':
         n1 = float(input('type first number: '))
         n2 = float(input('type second number: '))
         double_space()
-        print(f'{n1} / {n2} = ',division(n1,n2))
+        print(f'{n1} / {n2} = ',CalcuOptions.division(n1,n2))
         double_space()
     z = input(msg).lower()
